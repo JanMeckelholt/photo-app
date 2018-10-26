865aef9556fa04c787473d249349ad45d0f1b2a0
JanMeckelholt <Jan_Meckelholt@web.de>
2018-10-26 05:58:59 +0200

    separate pay-button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => 'registrations'}
+  get 'charges/new'
+  get 'charges/create'
+  devise_for :users #, :controllers => {:registrations => 'registrations'}
+  resources :charges
   root 'welcome#index'
   get '/welcome/something', to: 'welcome#something'
 
